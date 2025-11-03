240925a5d74a1b2326b61c3d1fdfb2e747c4ce22
hatake4911 <hatakeyamamitstumu1120@gmail.com>
2025-11-03 10:12:14 +0900

    a

--- Game2/js/input.js
+++
@@ -1,6 +0,0 @@
-export const keys = {};
-
-export function setupInput() {
-  window.addEventListener('keydown', e => { keys[e.code] = true; });
-  window.addEventListener('keyup', e => { keys[e.code] = false; });
-}
