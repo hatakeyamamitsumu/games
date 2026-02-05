e457209189e31d1ec3375b583a790a14f2449935
hatake4911 <hatakeyamamitstumu1120@gmail.com>
2026-02-05 13:16:55 +0900

    a

--- Jump_Game/js/render.js
+++ Jump_Game/js/render.js
@@ -30,9 +30,9 @@ import {
 
 } from "./enemy.js";
 
-// ▼ ブロック画像（1〜33）
+// ▼ ブロック画像（1〜54）
 const blockImages = [];
-for (let i = 1; i <= 33; i++) {
+for (let i = 1; i <= 54; i++) {
   const img = new Image();
   img.src = `./images/characters/block${i}.png`;
   blockImages.push(img);
